04014f07d8a88dae6105c47f4fa0ef43466acecd
Marcelo Díaz <mdiaz@digitalgeko.com>
2024-03-06 22:44:58 -0600

    Trabajando con estados

--- app/src/main/java/com/example/jetpackcomposecatalogo/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposecatalogo/MainActivity.kt
@@ -17,10 +17,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -39,10 +42,26 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MyComplexLayout()
+                    MyStateExample()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MyStateExample() {
+    var counter = remember {mutableStateOf(0)}
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { counter.value +=1 }) {
+            Text(text = "pulsar")
+        }
+        Text(text = "He sido pulsado ${counter.value} veces")
+
     }
 }
 
@@ -168,6 +187,6 @@ fun MyBox() {
 @Composable
 fun GreetingPreview() {
     JetPackComposeCatalogoTheme {
-        MyComplexLayout()
+        MyStateExample()
     }
 }
